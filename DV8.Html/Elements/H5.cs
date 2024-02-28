9b010a3533765bdf6c0d3b04d712bc3d3bb6ae4e
V.V. Shinkevich <v.v.shinkevich@gmail.com>
2024-02-28 16:33:35 +0300

    Add some missed tags

---
+++ DV8.Html/Elements/H5.cs
@@ -0,0 +1,14 @@
+﻿using DV8.Html.Framework;
+
+namespace DV8.Html.Elements;
+
+public class H5 : HtmlElement
+{
+    public H5()
+    {
+    }
+
+    public H5(string text) : base(null, text)
+    {
+    }
+}
---
+++ DV8.Html/Elements/H6.cs
@@ -0,0 +1,14 @@
+﻿using DV8.Html.Framework;
+
+namespace DV8.Html.Elements;
+
+public class H6 : HtmlElement
+{
+    public H6()
+    {
+    }
+
+    public H6(string text) : base(null, text)
+    {
+    }
+}
